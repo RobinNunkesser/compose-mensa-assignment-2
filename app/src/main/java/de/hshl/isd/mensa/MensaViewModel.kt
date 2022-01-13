e6e4db1c549049f4bc1584d853aa5dcb670a4bb2
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-13 14:00:50 +0100

    mutableStateOf

--- app/src/main/java/de/hshl/isd/mensa/MensaViewModel.kt
+++ app/src/main/java/de/hshl/isd/mensa/MensaViewModel.kt
@@ -1,8 +1,10 @@
 package de.hshl.isd.mensa
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MensaViewModel : ViewModel() {
-
-    var collections : List<CollectionViewModel> = listOf()
+    var collections by mutableStateOf(listOf<CollectionViewModel>())
 }
